63468d179d6257b9b348fa8c0e519ee88d68616d
zjjjjkkk <3348805775@qq.com>
2025-12-01 18:41:19 +0800

    "fix 修改oss文件上传问题"

--- src/components/Upload/singleUpload.vue
+++ src/components/Upload/singleUpload.vue
@@ -1,10 +1,11 @@
-<template> 
+<template>
   <div>
     <el-upload
-      :action="useOss?ossUploadUrl:minioUploadUrl"
-      :data="useOss?dataObj:null"
+      :action="useOss ? ossUploadUrl : minioUploadUrl"
+      :data="useOss ? dataObj : null"
       list-type="picture"
-      :multiple="false" :show-file-list="showFileList"
+      :multiple="false"
+      :show-file-list="showFileList"
       :file-list="fileList"
       :before-upload="beforeUpload"
       :on-remove="handleRemove"
@@ -18,104 +19,89 @@
     </el-dialog>
   </div>
 </template>
-<script>
-  import {policy} from '@/api/oss'
 
-  export default {
-    name: 'singleUpload',
-    props: {
-      value: String
+<script>
+// 注意：已移除 policy 导入（不再需要）
+
+export default {
+  name: 'singleUpload',
+  props: {
+    value: String
+  },
+  computed: {
+    imageUrl() {
+      return this.value;
     },
-    computed: {
-      imageUrl() {
-        return this.value;
-      },
-      imageName() {
-        if (this.value != null && this.value !== '') {
-          return this.value.substr(this.value.lastIndexOf("/") + 1);
-        } else {
-          return null;
-        }
-      },
-      fileList() {
-        return [{
-          name: this.imageName,
-          url: this.imageUrl
-        }]
-      },
-      showFileList: {
-        get: function () {
-          return this.value !== null && this.value !== ''&& this.value!==undefined;
-        },
-        set: function (newValue) {
-        }
+    imageName() {
+      if (this.value != null && this.value !== '') {
+        return this.value.substr(this.value.lastIndexOf("/") + 1);
+      } else {
+        return null;
       }
     },
-    data() {
-      return {
-        dataObj: {
-          policy: '',
-          signature: '',
-          key: '',
-          ossaccessKeyId: '',
-          dir: '',
-          host: '',
-          // callback:'',
-        },
-        dialogVisible: false,
-        useOss:false, //使用oss->true;使用MinIO->false
-        ossUploadUrl:'http://macro-oss.oss-cn-shenzhen.aliyuncs.com',
-        minioUploadUrl:'http://localhost:8080/minio/upload',
-      };
+    fileList() {
+      return [{
+        name: this.imageName,
+        url: this.imageUrl
+      }]
     },
-    methods: {
-      emitInput(val) {
-        this.$emit('input', val)
+    showFileList: {
+      get: function () {
+        return this.value !== null && this.value !== '' && this.value !== undefined;
       },
-      handleRemove(file, fileList) {
-        this.emitInput('');
-      },
-      handlePreview(file) {
-        this.dialogVisible = true;
-      },
-      beforeUpload(file) {
-        let _self = this;
-        if(!this.useOss){
-          //不使用oss不需要获取策略
-          return true;
-        }
-        return new Promise((resolve, reject) => {
-          policy().then(response => {
-            _self.dataObj.policy = response.data.policy;
-            _self.dataObj.signature = response.data.signature;
-            _self.dataObj.ossaccessKeyId = response.data.accessKeyId;
-            _self.dataObj.key = response.data.dir + '/${filename}';
-            _self.dataObj.dir = response.data.dir;
-            _self.dataObj.host = response.data.host;
-            // _self.dataObj.callback = response.data.callback;
-            resolve(true)
-          }).catch(err => {
-            console.log(err)
-            reject(false)
-          })
-        })
-      },
-      handleUploadSuccess(res, file) {
-        this.showFileList = true;
-        this.fileList.pop();
-        let url = this.dataObj.host + '/' + this.dataObj.dir + '/' + file.name;
-        if(!this.useOss){
-          //不使用oss直接获取图片路径
-          url = res.data.url;
-        }
-        this.fileList.push({name: file.name, url: url});
-        this.emitInput(this.fileList[0].url);
+      set: function (newValue) {
       }
     }
+  },
+  data() {
+    return {
+      // 注意：dataObj 已无实际作用（但保留结构避免报错）
+      dataObj: {
+        policy: '',
+        signature: '',
+        key: '',
+        ossaccessKeyId: '',
+        dir: '',
+        host: ''
+      },
+      dialogVisible: false,
+      useOss: true, // 使用 oss -> true; 使用 MinIO -> false
+      // ✅ 关键修改：OSS 上传地址指向后端接口
+      ossUploadUrl: 'http://localhost:8080/upload/oss',
+      minioUploadUrl: 'http://localhost:8080/minio/upload'
+    };
+  },
+  methods: {
+    emitInput(val) {
+      this.$emit('input', val)
+    },
+    handleRemove(file, fileList) {
+      this.emitInput('');
+    },
+    handlePreview(file) {
+      this.dialogVisible = true;
+    },
+    beforeUpload(file) {
+      // ✅ 关键修改：OSS 模式不再需要获取策略
+      if (this.useOss) {
+        return true // 直接允许上传（由后端处理）
+      }
+      // MinIO 模式保持原逻辑
+      return true
+    },
+    handleUploadSuccess(res, file) {
+      this.showFileList = true;
+      this.fileList.pop();
+
+      // ✅ 关键修改：统一从 res.data 获取 URL（适配后端返回格式）
+      let url = res.data;
+
+      this.fileList.push({ name: file.name, url: url });
+      this.emitInput(this.fileList[0].url);
+    }
   }
+}
 </script>
+
 <style>
-
 </style>
-
-
